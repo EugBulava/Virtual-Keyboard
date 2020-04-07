9af46497470e98e1cd69aef53b89d1b95fb6af5d
Yauheni_Bulava <jonbulova97@gmail.com>
2020-04-07 21:12:00 +0300

    feat: add eslint-config-airbnb-base

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "extends": "airbnb-base",
+
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        
+    }
+}
